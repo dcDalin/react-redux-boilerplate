99685d5b5ed34f550ca3d9c59fabf1de4ebf2ed0
dcDalin <mcdalinoluoch@gmail.com>
2018-04-16 00:17:21 +0300

    :arrow_upper_right: Add react-router-redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, history } from "./redux/store";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App history={history} />
   </Provider>,
   document.getElementById("root")
 );
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import { routerReducer } from "react-router-redux";
 import firstOne from "./firstOne";
 
 const rootReducer = combineReducers({
+  router: routerReducer,
   firstOne
 });
 
