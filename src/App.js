09534777ee517d5a2a9823211d2610e7dd5860ac
dcDalin <mcdalinoluoch@gmail.com>
2018-04-16 00:31:40 +0300

    [Chore] :new: Add React Router V4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import { ConnectedRouter } from "react-router-redux";
 
-const App = () => (
-  <div>
-    <h2>Working</h2>
-  </div>
-);
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+const App = props => {
+  const { history } = props;
+
+  return (
+    <ConnectedRouter history={history}>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+      </div>
+    </ConnectedRouter>
+  );
+};
 
 export default App;
